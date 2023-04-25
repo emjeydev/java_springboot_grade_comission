c9738455d7a5280a551e6a23d055acb14a9b2f82
EmJey <emjeydev@gmail.com>
2023-04-25 19:17:17 +0330

    add grade comission project

---
+++ src/main/java/com/ltp/gradesubmission/Grade.java
@@ -0,0 +1,41 @@
+package com.ltp.gradesubmission;
+
+public class Grade {
+    private  String name;
+    private String subject;
+    private String grade;
+
+    public Grade(String name, String subject, String grade) {
+        this.name = name;
+        this.subject = subject;
+        this.grade = grade;
+    }
+
+    public Grade() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+}
---
+++ src/main/java/com/ltp/gradesubmission/GradeContriller.java
@@ -0,0 +1,48 @@
+package com.ltp.gradesubmission;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class GradeContriller {
+
+    List<Grade> studentGrades = new ArrayList<>();
+
+    @GetMapping("/grades")
+    public String getGrades(Model model) {
+        model.addAttribute("grades", studentGrades);
+        return "grades";
+    }
+
+    @GetMapping("/form")
+    public String gradeForm(Model model, @RequestParam(required = false) String name) {
+        model.addAttribute("grade", getGradeIndex(name) == -1000 ? new Grade() : studentGrades.get(getGradeIndex(name)));
+        return "form";
+    }
+
+    @PostMapping("/handleSubmit")
+    public String submitForm(Grade grade) {
+        int index = getGradeIndex(grade.getName());
+
+        if (index == -1000) {
+            studentGrades.add(grade);
+        } else {
+            studentGrades.set(index, grade);
+        }
+
+        return "redirect:/grades";
+    }
+
+    public Integer getGradeIndex(String name) {
+        for (int i = 0; i < studentGrades.size(); i++) {
+            if (studentGrades.get(i).getName().equals(name)) return i;
+        }
+        return -1000;
+    }
+}
